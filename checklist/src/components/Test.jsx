fd3bf1017b062a30480c55da3b05d559d7e03fd1
Henrique <henrique.garcia.cap698@gmail.com>
2025-04-01 00:10:30 -0300

    Testando Componente Class

---
+++ checklist/src/components/Test.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+class Test extends React.Component{
+    constructor (props){
+        super(props);
+        this.state={
+            message: "",
+        };
+    }
+    render(){
+        return <h1></h1>
+    }
+}
+
+export default Test;
